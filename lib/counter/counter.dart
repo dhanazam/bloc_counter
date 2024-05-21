57b9f438decfae5e622d72bf2977dc394c3e111f
Ramadhan <ramadanazam93@gmail.com>
2024-05-22 03:00:23 +0800

    export files

--- lib/counter/counter.dart
+++ lib/counter/counter.dart
@@ -1,6 +1,2 @@
-import 'package:flutter/material.dart';
-
-class CounterApp extends MaterialApp {
-  const CounterApp({super.key}) : super(home: const CounterPage());
-  
-}
+export 'cubit/counter_cubit.dart';
+export 'view/view.dart';
